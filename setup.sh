05278f8cdc302b118cd0a533b2ff54bc18150a77
Tom Nagle <tomanagle@gmail.com>
2023-08-28 14:15:45 +1000

    feat: setup script

--- setup.sh
+++ setup.sh
@@ -24,7 +24,6 @@ generateKeys(){
 
 generateKeys
 
-
 replace_module_name() {
     echo "Enter your module name (e.g., github.com/yourusername/yourmodule):"
     read new_module_name
@@ -34,8 +33,18 @@ replace_module_name() {
         exit 1
     fi
 
-    # Replace old module name with the new one in all files
-    find . -type f -exec sed -i "s/github\.com\/tomdoestech\/goth/$new_module_name/g" {} +
+    echo "You entered: $new_module_name"
+    echo "Are you sure you want to replace 'github.com/tomdoestech/goth2' with '$new_module_name'? (y/n):"
+    read confirmation
+
+    if [[ "$confirmation" != "y" && "$confirmation" != "Y" ]]; then
+        echo "Operation canceled."
+        exit 0
+    fi
+
+    # Escape the delimiter in the module name and use it as the delimiter for sed
+    escaped_module_name=$(echo "$new_module_name" | sed 's/[\/&]/\\&/g')
+    find . -type f -exec sed -i "s|github\.com/tomdoestech/goth|$escaped_module_name|g" {} +
 
     echo "Module name replaced in all files."
 }
